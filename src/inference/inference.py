ecd4c9e0bf9637ef261d415f8c2323c3844976df
GL26 <gauthier.lechevalier@student-cs.fr>
2025-03-18 15:14:24 +0100

    code restructuration

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -4,119 +4,146 @@ from ..lib.potential import Potential
 from scipy.optimize import curve_fit
 
 
-pot1 = Potential(difficulty=1, random=False)
+class DataGenerator:
+    def __init__(self, distribution: Potential, n=100):
+        self.pot = distribution
+        x_moy, y_moy, sigma_1, sigma_2 = self.get_true_parameters()
+        self.xmoy = x_moy
+        self.ymoy = y_moy
+        self.sigma_1 = sigma_1
+        self.sigma_2 = sigma_2
+        self.n = n
+        return
 
-fig2, ax2 = pot1.plot(2)
+    def get_true_parameters(self):
+        pot = self.pot
+        moy_1 = pot.mu1
+        x_moy = moy_1[0]
+        y_moy = moy_1[1]
+        sigma_1 = pot.gaussian1.cov[0, 0]
+        sigma_2 = pot.gaussian1.cov[1, 1]
+        return x_moy, y_moy, sigma_1, sigma_2
 
-epsilon = 1.0
+    def generate_n_points(self):
+        n = self.n
+        L = []
+        for i in range(n):
+            L.append(
+                [
+                    np.random.uniform(self.pot.xmin, self.pot.xmax),
+                    np.random.uniform(self.pot.ymin, self.pot.ymax),
+                ]
+            )
+        return L
+
+    def evaluate_n_points(self):
+        n = self.n
+
+        return_list = []
+        L = self.generate_n_points()
+        for i in range(n):
+            return_list.append(self.pot.value(L[i]))
+        return L, return_list
+
+    def evaluate_n_points_from_list(self, L):
+        return_list = []
+        for i in range(len(L)):
+            return_list.append(self.pot.value(L[i]))
+        return return_list
+
+    def generate_n_points_noise(self):
+        L, Ly = self.evaluate_n_points()
+        for i in range(len(Ly)):
+            Ly[i] += np.random.normal()
+        return L, Ly
+
+    def gen_data(self):
+        L, Ly_noise = self.generate_n_points_noise()
+        L = np.array(L)
+        Ly_true = self.evaluate_n_points_from_list(L)
+        Ly_true = np.array(Ly_true)
+        return L, Ly_true
 
 
-def get_true_parameters(pot: Potential):
-    moy_1 = pot.mu1
-    x_moy = moy_1[0]
-    y_moy = moy_1[1]
-    sigma_1 = pot.gaussian1.cov[0, 0]
-    sigma_2 = pot.gaussian1.cov[1, 1]
-    return x_moy, y_moy, sigma_1, sigma_2
+class LawEstimator:
+    def __init__(self, data_generator: DataGenerator):
+        self.data_generator = data_generator
+        self.L, self.Ly_true = self.data_generator.gen_data()
+        return
 
+    def diff(self, L, Ly_noise):
+        Ly_true = self.data_generator.evaluate_n_points_from_list(L)
+        sum = 0
+        for i in range(len(L)):
+            sum += (Ly_noise[i] - Ly_true[i]) ** 2
+        return sum
 
-x_moy, y_moy, sigma_1, sigma_2 = get_true_parameters(pot1)
-print(x_moy, y_moy)
-print(sigma_1)
-print(sigma_2)
-
-
-def generate_n_points(n: int):
-    L = []
-    for i in range(n):
-        L.append(
-            [
-                np.random.uniform(pot1.xmin, pot1.xmax),
-                np.random.uniform(pot1.ymin, pot1.ymax),
-            ]
+    def twoD_Gaussian(
+        self,
+        coordinates,
+        amplitude,
+        xo,
+        yo,
+        sigma_x,
+        sigma_y,
+        theta,
+        offset,
+    ):
+        x, y = coordinates
+        xo = float(xo)
+        yo = float(yo)
+        # Calculate the rotation components
+        a = (np.cos(theta) ** 2) / (2 * sigma_x**2) + (np.sin(theta) ** 2) / (
+            2 * sigma_y**2
         )
-    return L
-
-
-def evaluate_n_points(n: int):
-    return_list = []
-    L = generate_n_points(n=n)
-    for i in range(n):
-        return_list.append(pot1.value(L[i]))
-    return L, return_list
-
-
-def evaluate_n_points_from_list(L):
-    return_list = []
-    for i in range(len(L)):
-        return_list.append(pot1.value(L[i]))
-    return return_list
-
-
-def generate_n_points_noise(n: int):
-    L, Ly = evaluate_n_points(10)
-    for i in range(len(Ly)):
-        Ly[i] += np.random.normal()
-    return L, Ly
-
-
-L, Ly_noise = generate_n_points_noise(100)
-L = np.array(L)
-Ly_true = evaluate_n_points_from_list(L)
-Ly_true = np.array(Ly_true)
-
-
-def diff(L, Ly_noise):
-    Ly_true = evaluate_n_points_from_list(L)
-    sum = 0
-    for i in range(len(L)):
-        sum += (Ly_noise[i] - Ly_true[i]) ** 2
-    return sum
-
-
-L = L.transpose()
-data = Ly_true[:].reshape(1, len(Ly_true))
-print(L)
-print(data)
-
-# Initial guess for parameters
-initial_guess = (1, 0, 0, 1, 1, 0, 0)
-
-
-# Define the 2D Gaussian function
-def twoD_Gaussian(coordinates, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
-    x, y = coordinates
-    xo = float(xo)
-    yo = float(yo)
-    # Calculate the rotation components
-    a = (np.cos(theta) ** 2) / (2 * sigma_x**2) + (np.sin(theta) ** 2) / (
-        2 * sigma_y**2
-    )
-    b = -np.sin(2 * theta) / (4 * sigma_x**2) + np.sin(2 * theta) / (4 * sigma_y**2)
-    c = (np.sin(theta) ** 2) / (2 * sigma_x**2) + (np.cos(theta) ** 2) / (
-        2 * sigma_y**2
-    )
-    # Compute the Gaussian function
-    g = offset + np.log10(
-        amplitude
-        * np.exp(
-            -(a * ((x - xo) ** 2) + 2 * b * (x - xo) * (y - yo) + c * ((y - yo) ** 2))
+        b = -np.sin(2 * theta) / (4 * sigma_x**2) + np.sin(2 * theta) / (4 * sigma_y**2)
+        c = (np.sin(theta) ** 2) / (2 * sigma_x**2) + (np.cos(theta) ** 2) / (
+            2 * sigma_y**2
         )
-    )
-    return g.ravel()  # Flatten the 2D array to 1D for curve_fit
+        # Compute the Gaussian function
+        g = offset + np.log10(
+            amplitude
+            * np.exp(
+                -(
+                    a * ((x - xo) ** 2)
+                    + 2 * b * (x - xo) * (y - yo)
+                    + c * ((y - yo) ** 2)
+                )
+            )
+        )
+        return g.ravel()  # Flatten the 2D array to 1D for curve_fit
+
+    def fit(self):
+        L = self.L.transpose()
+        data = self.Ly_true[:].reshape(1, len(self.Ly_true))
+        initial_guess = (1, 0, 0, 1, 1, 0, 0)
+
+        popt, pcov = curve_fit(self.twoD_Gaussian, L, data.ravel(), p0=initial_guess)
+
+        popt_dict = {
+            "amplitude": popt[0],
+            "xo": popt[1],
+            "yo": popt[2],
+            "sigma_x": popt[3],
+            "sigma_y": popt[4],
+            "theta": popt[5],
+            "offset": popt[6],
+        }
+        self.popt_dict = popt_dict
+        print("Fitted parameters:", popt_dict)
+        return self.popt_dict
 
 
-# Perform the curve fitting
-popt, pcov = curve_fit(twoD_Gaussian, L, data.ravel(), p0=initial_guess)
+def main():
+    pot1 = Potential(difficulty=1, random=False)
+    fig2, ax2 = pot1.plot(2)
 
-popt_dict = {
-    "amplitude": popt[0],
-    "xo": popt[1],
-    "yo": popt[2],
-    "sigma_x": popt[3],
-    "sigma_y": popt[4],
-    "theta": popt[5],
-    "offset": popt[6],
-}
+    dg = DataGenerator(pot1, n=100)
 
-print("Fitted parameters:", popt_dict)
+    le = LawEstimator(data_generator=dg)
+    le.fit()
+    return
+
+
+if __name__ == "__main__":
+    main()
